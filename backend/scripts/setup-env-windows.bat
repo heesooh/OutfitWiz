9218f377f32f10fc51b7c164fb0e4041b343f87b
Heesoo Hwang <heesoohwang1231@gmail.com>
2024-04-01 19:26:59 -0700

    Updated the backend initialization code.

--- backend/scripts/setup-env-windows.bat
+++ backend/scripts/setup-env-windows.bat
@@ -2,15 +2,21 @@
 
 cd ..
 
+echo Activating Miniconda3...
+echo If you face any issue, check if the Miniconda3 path is correct
+echo You can find your Miniconda3 path with "where conda" on CMD
 call C:\ProgramData\miniconda3\Scripts\activate.bat
 
 echo Creating Conda environment...
-conda create -n outfitwiz_env python=3.8
+call conda create -n outfitwiz_env python=3.8
 
 echo Activating Conda environment...
 call conda activate outfitwiz_env 
 
+echo Installing Pytorch...
+call pip install torch==2.2.1+cu121 -f https://download.pytorch.org/whl/torch_stable.html
+
 echo Installing requirements...
-pip install -r requirements.txt
+call pip install -r requirements.txt
 
 pause
